928052f4d4d09686d3b01b4e1ee25351a9bf3f92
Nolan Barker <fpsbarkern17@stu.fpsk.12.net>
2024-01-11 19:15:13 -0500

    Removed unnecesary imports in PhotonVision.java

--- 2024-CRESCENDO/src/main/java/frc/robot/subsystems/PhotonVision.java
+++ 2024-CRESCENDO/src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -14,8 +14,6 @@ import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
