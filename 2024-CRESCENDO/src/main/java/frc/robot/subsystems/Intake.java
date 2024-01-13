87e2c9120dae2ddcc781181a36b42a1136c06fd4
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2024-01-12 20:03:22 -0500

    Intake

---
+++ 2024-CRESCENDO/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Intake extends SubsystemBase {
+
+  TalonFX intakeMotor;
+
+  public Intake() {
+    intakeMotor = new TalonFX(Constants.IntakeConstants.intakeMotorID);
+    intakeMotor.clearStickyFaults();
+    intakeMotor.getConfigurator().apply(new TalonFXConfiguration());
+    intakeMotor.setInverted(false);
+  }
+
+  @Override
+  public void periodic() {
+  }
+
+  public void setMotor(double speed) {
+    intakeMotor.set(speed);
+  }
+
+  public void stopMotor() {
+    intakeMotor.set(0);
+  }
+  
+}
