f5d73743de3edc8fc936a88a8ad295f5a729a80f
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2024-01-12 19:45:30 -0500

    ShooterCommand

---
+++ 2024-CRESCENDO/src/main/java/frc/robot/commands/ShooterCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Shooter;
+
+public class ShooterCommand extends Command {
+
+  Shooter m_Shooter = new Shooter();
+  double speed;
+
+  public ShooterCommand(Shooter m_Shooter, double speed) {
+    addRequirements(m_Shooter);
+    this.m_Shooter = m_Shooter;
+    this.speed = speed;
+  }
+
+  @Override
+  public void initialize() {
+    m_Shooter.setMotor(speed);
+  }
+
+  @Override
+  public void execute() {}
+
+  @Override
+  public void end(boolean interrupted) {
+    m_Shooter.stopMotor();
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
