f891829e48ce26277b665359993067e55420a935
AkshayKumar41 <akskum700@gmail.com>
2024-01-28 14:42:53 -0500

    removed sysout

--- 2024-CRESCENDO/src/main/java/frc/robot/subsystems/Elevator.java
+++ 2024-CRESCENDO/src/main/java/frc/robot/subsystems/Elevator.java
@@ -124,7 +124,6 @@ public class Elevator extends ProfiledPIDSubsystem implements AutoCloseable {
     elevatorPosition = elevatorMotor.getPosition().getValueAsDouble();
     elevatorCanCoderVelocity = elevatorCanCoder.getVelocity().getValueAsDouble();
     elevatorCanCoderPosition = elevatorCanCoder.getAbsolutePosition().getValueAsDouble();
-    System.out.println(elevatorPosition);
   }
 
   @Override
