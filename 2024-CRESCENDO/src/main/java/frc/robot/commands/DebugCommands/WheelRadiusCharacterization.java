2c2f6a9d566e4a209fe2278a587a36ec88b50921
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2024-03-06 20:07:28 -0500

    Compute DriveBase Radius

--- 2024-CRESCENDO/src/main/java/frc/robot/commands/DebugCommands/WheelRadiusCharacterization.java
+++ 2024-CRESCENDO/src/main/java/frc/robot/commands/DebugCommands/WheelRadiusCharacterization.java
@@ -1,21 +1,26 @@
 package frc.robot.commands.DebugCommands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class WheelRadiusCharacterization extends Command {
   CommandSwerveDrivetrain drivetrain;
 
-
   public WheelRadiusCharacterization(CommandSwerveDrivetrain drivetrain) {
 
     addRequirements(drivetrain);
     this.drivetrain = drivetrain;
-    
+
   }
 
   @Override
   public void initialize() {
+    double driveBaseRadius = 0;
+    for (var moduleLocation : drivetrain.moduleLocations()) {
+      driveBaseRadius = Math.max(driveBaseRadius, moduleLocation.getNorm());
+    }
+    SmartDashboard.putNumber("drivebase radius", driveBaseRadius);
   }
 
   @Override
