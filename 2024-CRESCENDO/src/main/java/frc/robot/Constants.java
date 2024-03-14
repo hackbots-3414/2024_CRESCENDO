a84f9f1edbe98d433ee8a1c4bd4aefd4d0a22691
Vijay Nannapuraju <59799317+vijayn7@users.noreply.github.com>
2024-03-14 19:55:49 -0400

    AmpSide5Piece Auton

--- 2024-CRESCENDO/src/main/java/frc/robot/Constants.java
+++ 2024-CRESCENDO/src/main/java/frc/robot/Constants.java
@@ -313,8 +313,8 @@ public class Constants {
             public static final double kS = 0.0; // output to overcome static friction (output)
             public static final double kV = 12.0; // output per unit of target velocity (output/rps)
             public static final double kA = 0.0; // output per unit of target acceleration (output/(rps/s))
-            // public static final double kG = 0.15; // feedforward Constant
-            public static final double kG = 0.0;
+            public static final double kG = 0.15; // feedforward Constant
+            // public static final double kG = 0.0;
         }
 
         public static final class PivotMotionMagicConstants {
