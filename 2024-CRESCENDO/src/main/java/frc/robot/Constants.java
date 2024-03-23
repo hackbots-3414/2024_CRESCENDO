a85cb3e26d9a4efdc429a44f52546b6f2846f826
alexjkarki <148502386+alexjkarki@users.noreply.github.com>
2024-03-23 08:23:11 -0400

    switched to PS5 controller

--- 2024-CRESCENDO/src/main/java/frc/robot/Constants.java
+++ 2024-CRESCENDO/src/main/java/frc/robot/Constants.java
@@ -199,7 +199,7 @@ public class Constants {
 
         public static final double expoPower = 2.0;
 
-        public static final JoystickChoice operatorController = JoystickChoice.XBOX;
+        public static final JoystickChoice operatorController = JoystickChoice.PS5;
         public static final AutonViews autonView = AutonViews.AMP;
     }
 
