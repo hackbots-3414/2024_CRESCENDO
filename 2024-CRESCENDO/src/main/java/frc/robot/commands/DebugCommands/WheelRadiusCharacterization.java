631f6bf3ed4790e5ff81917a9faa0f05d7d4864a
Nolan <nolanb414@yahoo.com>
2024-03-31 21:43:57 -0400

    mod 2 fails in sim

--- 2024-CRESCENDO/src/main/java/frc/robot/commands/DebugCommands/WheelRadiusCharacterization.java
+++ 2024-CRESCENDO/src/main/java/frc/robot/commands/DebugCommands/WheelRadiusCharacterization.java
@@ -6,6 +6,7 @@ import java.math.RoundingMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
@@ -18,9 +19,9 @@ public class WheelRadiusCharacterization extends Command {
   private CommandSwerveDrivetrain drivetrain;
 
   private SwerveRequest request = new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage)
-    .withVelocityX(0)
+    .withVelocityX(1)
     .withVelocityY(0)
-    .withRotationalRate(1);
+    .withRotationalRate(0);
   
   private double[] initialWheelRotation = new double[4];
   private double[] finalWheelRotation = new double[4];
@@ -30,6 +31,8 @@ public class WheelRadiusCharacterization extends Command {
   private double initialGyroDegrees;
   private double finalGyroDegrees;
   private BigDecimal drivebaseRotations;
+
+  private BigDecimal fullRotationDegrees = new BigDecimal(360);
 
   private int precision = 12; // higher is more precise
 
@@ -44,7 +47,7 @@ public class WheelRadiusCharacterization extends Command {
   public void initialize() {
       for (int i = 0;i < 4;i ++) {
         TalonFX driveMotor = drivetrain.getModule(i).getDriveMotor();
-        
+
         initialWheelRotation[i] = driveMotor.getPosition().getValueAsDouble();
 
         drivebaseRadii[i] = drivetrain.moduleLocations()[i].getNorm();
@@ -56,8 +59,16 @@ public class WheelRadiusCharacterization extends Command {
 
   @Override
   public void execute() {
-      drivetrain.setControl(request);
-      logger.debug("applied request");
+    drivetrain.setControl(request);
+    logger.debug("applied request");
+    
+    drivebaseRotations = new BigDecimal(drivetrain.getPigeon2().getAngle()).divide(fullRotationDegrees);
+
+    // update each wheel's position
+    for (int i = 0;i < 4;i ++) {
+      finalWheelRotation[i] = drivetrain.getModule(i).getDriveMotor().getPosition().getValueAsDouble();
+      SmartDashboard.putNumber("mod " + i + " rotation", finalWheelRotation[i] - initialWheelRotation[i]);
+    }
   }
 
   @Override
@@ -81,8 +92,6 @@ public class WheelRadiusCharacterization extends Command {
   private BigDecimal getDriveBaseRotations() {
     finalGyroDegrees = drivetrain.getPigeon2().getAngle();
 
-    BigDecimal fullRotationDegrees = new BigDecimal(360);
-
     BigDecimal initialGyroRotations = new BigDecimal(initialGyroDegrees).divide(fullRotationDegrees, precision, RoundingMode.HALF_UP);
     BigDecimal finalGyroRotations = new BigDecimal(finalGyroDegrees).divide(fullRotationDegrees, precision, RoundingMode.HALF_UP);
 
@@ -96,11 +105,11 @@ public class WheelRadiusCharacterization extends Command {
   private BigDecimal calculateWheelRadius(int index) {
     BigDecimal drivebaseRadius = new BigDecimal(drivebaseRadii[index]);
 
-    BigDecimal wheelRotations = new BigDecimal(finalWheelRotation[index]).subtract(new BigDecimal(initialWheelRotation[index]));
+    BigDecimal wheelRotations = new BigDecimal(finalWheelRotation[index]).subtract(new BigDecimal(initialWheelRotation[index])).abs();
 
     BigDecimal wheelRadius = drivebaseRadius.multiply(drivebaseRotations).divide(wheelRotations, precision, RoundingMode.HALF_UP);
 
-    logger.info("wheel radius for module " + index);
+    logger.info("wheel radius for module " + index + ": " + wheelRadius.toPlainString());
 
     return wheelRadius;
 
